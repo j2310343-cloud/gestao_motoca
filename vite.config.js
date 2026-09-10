d2a2b2f6daddc961d3f2474ba22030599646933d
j2310343-cloud <j2310343@gmail.com>
2026-09-10 11:07:52 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Substitua 'NOME-DO-SEU-REPOSITORIO' pelo nome exato do seu repositório no GitHub
 export default defineConfig({
   plugins: [react()],
+  base: '/gestao_motoca/', 
 })
